043b04cb66244ae4b9127d69eb78665dc22b3358
Neil Davis <neildavis@neils-mbp.home>
2021-01-24 13:34:01 +0000

    context API

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,44 +3,48 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+// this creates 2 components - 'Provider' and 'Consumer' as part of the create context
+
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
-  const removePerson = (id) => {
-    setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
-  return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
+    // route component
+    const [people, setPeople] = useState(data);
+    const removePerson = (id) => {
+        setPeople((people) => {
+            return people.filter((person) => person.id !== id);
+        });
+    };
+    return (
+        <PersonContext.Provider value={{ removePerson, people }}>
+            {/* return must be wrapped in PersonContext.Provider at the top of the APP tree */}
+            <>
+                <h3>Context API</h3>
+                <List/>
+            </>
+        </PersonContext.Provider>
+    );
 };
 
-const List = ({ people, removePerson }) => {
-  return (
-    <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
-      })}
-    </>
-  );
+const List = () => {
+    // No need for this component to know about removePerson() anymore
+    const { people } = useContext(PersonContext);
+    return (
+        <>
+            {people.map((person) => {
+                return <SinglePerson key={person.id} {...person} />;
+            })}
+        </>
+    );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
-  return (
-    <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>remove</button>
-    </div>
-  );
+const SinglePerson = ({ id, name }) => {
+    const { removePerson } = useContext(PersonContext); // can access method directly storing in a variable
+    return (
+        <div className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+        </div>
+    );
 };
 
 export default ContextAPI;
