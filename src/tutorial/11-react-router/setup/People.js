fc457de7b9c4fd7d0f4780c166f0d3710e18b83f
Neil Davis <neildavis@neils-mbp.home>
2021-01-26 22:40:51 +0000

    react router, switch, links...

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -8,10 +8,11 @@ const People = () => {
       <h1>People Page</h1>
       {people.map((person) => {
         return (
-          <div key={person.id} className='item'>
-            <h4>{person.name}</h4>
-          </div>
-        );
+            <div key={person.id} className="item">
+                <h4>{person.name}</h4>
+                <Link to={`/person/${person.id}`}>view</Link>
+            </div>
+        )
       })}
     </div>
   );
--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,12 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { data } from '../../../data'
+import { Link, useParams } from 'react-router-dom'
 const Person = () => {
-  return (
-    <div>
-      <h2>person</h2>
-    </div>
-  );
-};
+  const [name, setName] = useState('Default')
+    const {id} = useParams()
+    useEffect(() => {
+        const { name } = data.find((person) => {
+            return person.id === parseInt(id)
+        })
+        setName(name)
+    }, [])
+    return (
+        <div>
+            <h2>person</h2>
+            <h4>{name}</h4>
+            <Link to='/people' className='btn'>Back to People</Link>
+        </div>
+    )
+}
 
-export default Person;
+export default Person
